6a4d164bb75e1e7d787eb4edcafbd7da00286d6a
Marcin Borkowski <mbork@mbork.pl>
2015-08-30 21:44:02 +0200

    Add check for slug property for submitting on done

--- beeminder.el
+++ beeminder.el
@@ -992,7 +992,8 @@ to the slug of the goal), the \"amount\" property (defaults to 1), the
 				    ""))
 		      "done")
 	     (eq (plist-get state-change :type) 'todo-state-change)
-	     (member (plist-get state-change :to) org-done-keywords))
+	     (member (plist-get state-change :to) org-done-keywords)
+	     (org-entry-get position "slug" beeminder-org-inherit-beeminder-properties))
 	(let* ((slug-str (org-entry-get position
 					"slug"
 					beeminder-org-inherit-beeminder-properties))
