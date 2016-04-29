588da278b524315a9003c33218d8173c2bb6dcaf
Marcin Borkowski <mbork@mbork.pl>
2016-04-29 07:17:39 +0200

    Fix a bug with "(" at the bol
    Opening paren at the beginning of line messes up Emacs code
    navigation...

--- beeminder.el
+++ beeminder.el
@@ -1060,11 +1060,11 @@ end."
 (defun beeminder-reload-goals-list (&optional force)
   "Reload the goals from the server.
 With a prefix argument, do it even if reloading is in progress
-(this is useful when the `beeminder-reloading-in-progress'
+\(this is useful when the `beeminder-reloading-in-progress'
 variable is somehow messed up)."
   (interactive "P")
   (if (and beeminder-reloading-in-progress
-	   (not force))
+           (not force))
       (beeminder-log "fetching goals already in progress, please wait!")
     (setq beeminder-reloading-in-progress t)
     (beeminder-get-goals)))
